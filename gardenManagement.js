3339fc0ec1ae282f87ea0209a9038c5b5ef51946
Ronny Nyabuto <ronnyabuto@icloud.com>
2025-12-05 12:22:44 +0300

    implement garden control logic

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,23 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+// 1. Watering Control
+if (temperature > 80) {
+    console.log("Watering on");
+} else {
+    console.log("Watering off");
+}
+
+// 2. Garden Lights Control
+if (timeOfDay === "evening" || timeOfDay === "night") {
+    console.log("Lights on");
+} else {
+    console.log("Lights off");
+}
+
+// 3. Soil Moisture Adjustment
+while (soilMoisture < 40) {
+    soilMoisture += 5;
+    console.log(soilMoisture);
+}
